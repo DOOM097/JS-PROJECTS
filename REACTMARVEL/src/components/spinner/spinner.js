e0aad4b2ff33605dec6c7a4406d72a0ba52f407c
DOOM097 <98549250+DOOM097@users.noreply.github.com>
2023-09-27 12:54:50 +0300

    Add files via upload
    SCRIP-9

--- REACTMARVEL/src/components/spinner/spinner.js
+++ REACTMARVEL/src/components/spinner/spinner.js
@@ -4,7 +4,7 @@ import './spinner.css';
 const Spinner = () => {
     return (
         <div className="lds-css ng-scope">
-            <div className="lds-spin"><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div></div>
+            
         </div>
     )
 }
